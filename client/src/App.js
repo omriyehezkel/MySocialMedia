17294ffcfe62e4c9cff0e61fdfe63b95e450d89d
omriyehezkel <40993248+omriyehezkel@users.noreply.github.com>
2023-03-29 20:45:06 +0300

    navBarCommit

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
-import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
-
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./state/theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
-  const mode = useS
   return (
     <div className="app">
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<LoginPage />} />
-  <Route path="/home" element={<HomePage />} />
-  <Route path="/profile/:userId" element={<ProfilePage />} />//parameter for who user login
-    
-</Routes>
-</BrowserRouter>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />} //parameter for who user login
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,22 @@
-const Navbar = () => {
-    return (<div> navbar</div>);
-};
-export default Navbar;
+// import { useState } from "react";
+// import { Box,IconButton,InputBase,Typography,Select,MenuItem,FormControl,useTheme,useMediaQuery }
+//  from "@mui/material";
+
+// import {
+//     Search,Message,DarkMode,LightMode,Notifications,Help,Menu,Close} from "@mui/icons-material";
+
+// import { useDispatch,useSelector } from "react-redux";
+// import state, { setMode,setLogout } from "state";
+// import { useNavigate } from "react-router-dom";
+// import FlexBetween from "components/FlexBetween";
+
+// const Navbar = () => {
+
+//     const[isMobileMenuToggled,setMobileMenuToggled]=useState(false);
+//     const dispatch = useDispatch();
+//     const navigate = useNavigate();
+//     const user = useSelector (state) =>state 
+
+//     return (<div> navbar</div>);
+// };
+// export default Navbar;
