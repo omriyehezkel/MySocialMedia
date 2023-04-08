3c73d76ae85b03b2a49d29e4a17c44f0ff2b46ec
omriyehezkel <40993248+omriyehezkel@users.noreply.github.com>
2023-04-08 15:58:53 +0300

    modified:   src/App.js
    Changes not staged for commit:
    	modified:   ../server/package.json

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  // const isAuth = Boolean(useSelector((state) => state.token));
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -22,11 +22,11 @@ function App() {
             <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
-              element={<HomePage />}
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
             <Route
               path="/profile/:userId"
-              element={<ProfilePage />}
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
             />
           </Routes>
         </ThemeProvider>
